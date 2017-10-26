f96e1baba5e0ddae05cc5871fed2c602f847bc4a
Harry Archer Blackburn <harryblackburn@Harrys-MacBook-Pro.local>
2017-10-26 16:29:02 +0100

    solution added

---
+++ 8kyu/Removing elements.js
@@ -0,0 +1,14 @@
+/* Take an array and remove every second element out of that array. Always keep the first element and start removing with the next element.
+
+Example:
+
+myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...]; */
+
+function removeEveryOther(arr){
+    var newArr=[];
+  for (var i = 0; i < arr.length; i+=2){
+    newArr.push(arr[i]);
+    }
+  return newArr;
+}
+  

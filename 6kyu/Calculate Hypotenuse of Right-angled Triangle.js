4985f8ddcc425354d5770c68f8fde1d85e0fa9dc
Harry Archer Blackburn <harryblackburn@Harrys-MacBook-Pro.local>
2017-10-28 12:31:33 +0100

    solution added

---
+++ 6kyu/Calculate Hypotenuse of Right-angled Triangle.js
@@ -0,0 +1,21 @@
+
+/* To solve this Kata, complete the function, calculateHypotenuse(a,b), which will return the length of the hyptenuse for a right angled triangle with the other two sides having a length equal to the inputs. More details:
+
+The returned value should be a number rounded to three decimal places
+An error (ArgumentException in C#) should be thrown if an invalid input is provided (inputs should both be numbers that are above zero)
+calculateHypotenuse(1,1); // returns 1.414
+calculateHypotenuse(3,4); // returns 5
+calculateHypotenuse(-2,1); // throws error
+calculateHypotenuse("one", "two"); // throws error
+*/
+
+function calculateHypotenuse(a, b) {
+    if (a === true || b === true) {
+      throw "error";
+    }
+    else if (a > 0 && b > 0) {
+      return Math.round(Math.sqrt(a * a + b * b) * 1000) / 1000;
+    } else {
+      throw "error";
+    }
+}

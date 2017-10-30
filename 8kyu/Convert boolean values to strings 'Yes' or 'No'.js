53580748b0c864c88e3441232e2570b2df227b2c
Harry Archer Blackburn <harryblackburn@Harrys-MacBook-Pro.local>
2017-10-30 09:53:18 +0000

    solution added

---
+++ 8kyu/Convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,13 @@
+/*  Complete the bool_to_word (PHP: boolToWord ) method.
+
+Given: a boolean value
+
+Return: a 'Yes' string for true and a 'No' string for false
+*/
+
+function boolToWord( bool ){
+    if (bool){
+    return "Yes"; 
+    }
+    return "No";
+}
